cf8747e8bdb04eb64e3349c8a5b1c3fc26a70baa
kimyk0101 <kimyk7131@gmail.com>
2024-12-23 12:48:12 +0900

    NavLink

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import { Routes, Route } from "react-router-dom";
 import Home from "/src/pages/Home";
 import About from "/src/pages/About";
 import Contact from "/src/pages/Contact";
-import Dashboard from "/src/pages/Dashboard";
+import Dashboard from "/src/pages/Dashboard/Dashboard";
 import Header from "../src/components/Header.jsx";
 import Navbar from "../src/components/Navbar.jsx";
+import Profile from "/src/pages/Dashboard/Profile";
+import Settings from "/src/pages/Dashboard/Settings";
+import "./routing.css";
 function App() {
   return (
     <div>
@@ -17,7 +20,11 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          {/* Nested Routing */}
+          <Route path="/dashboard" element={<Dashboard />}>
+            <Route path="profile/:userid" element={<Profile />} />
+            <Route path="settings" element={<Settings />} />
+          </Route>
         </Routes>
       </article>
     </div>
